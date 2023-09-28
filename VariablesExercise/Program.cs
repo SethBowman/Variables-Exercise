20e59150727bc29730652bc5a6b0fa98aab44ca6
SethBowman <brandonsethbowman@gmail.com>
2023-09-28 13:36:11 -0500

    Exercise completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //string myName = "Seth";
+            //int myAge = 29;
+            //double avg = 98.7;
+            //decimal myWallet = 200.00m;
+            //bool isMarried = true;
+            //char inital = 'S';
+
+            //Console.WriteLine($"Name: {myName}\nAge: {myAge}\nAverage: {avg}\nMoney: ${myWallet}\nMarried: {isMarried}\nInitial: {inital}");
+
+
+            Console.WriteLine("Enter your name:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
+
         }
     }
 }
